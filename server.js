635fe7c56af6a21e786b40ce9ee6f851e181bd29
Nyaarium <project@nyaarium.com>
2023-10-03 20:26:30 -0700

    switched to node-fetch

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const { parse } = require("url");
 const next = require("next");
 const path = require("path");
 const fs = require("fs");
+const nodeFetch = require("node-fetch");
 
 const dev = process.env.NODE_ENV !== "production";
 if (dev) {
@@ -46,10 +47,13 @@ function routeHandler(req, res, parsedUrl, next) {
 				let rawQuery = req.url.split("?")[1] ?? "";
 				if (rawQuery) rawQuery = `?${rawQuery}`;
 
-				return fetch(`http://localhost:${port}/api/file${rawQuery}`, {
-					method: "post",
-					body: JSON.stringify({ path: filePath }),
-				})
+				return nodeFetch(
+					`http://localhost:${port}/api/file${rawQuery}`,
+					{
+						method: "post",
+						body: JSON.stringify({ path: filePath }),
+					},
+				)
 					.then((fetchRes) => {
 						return fetchRes.body.pipe(res);
 					})
