ff0c2d0fa757ad6d0df17775b2e227abd716f3d2
Nyaarium <project@nyaarium.com>
2023-10-03 21:37:40 -0700

    docs

--- src/assets/client/sha256.js
+++ src/assets/client/sha256.js
@@ -1,5 +1,12 @@
 import { createHash } from "crypto";
 
+/**
+ * Generates a SHA-256 hash of the provided data.
+ *
+ * @param {string} data - The data to be hashed.
+ *
+ * @returns {string} The generated SHA-256 hash.
+ */
 export default function sha256(data) {
 	const hash = createHash("sha256");
 	hash.update(data);
--- src/assets/client/sha512.js
+++ src/assets/client/sha512.js
@@ -1,5 +1,12 @@
 import { createHash } from "crypto";
 
+/**
+ * Generates a SHA-512 hash of the provided data.
+ *
+ * @param {string} data - The data to be hashed.
+ *
+ * @returns {string} The generated SHA-512 hash.
+ */
 export default function sha512(data) {
 	const hash = createHash("sha512");
 	hash.update(data);
