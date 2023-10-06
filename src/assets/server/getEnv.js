d9aea33dbc4b2ce69e6508fc2d21407eed2bcb2b
Nyaarium <project@nyaarium.com>
2023-10-05 17:38:24 -0700

    improved getEnv

--- src/assets/server/getEnv.js
+++ src/assets/server/getEnv.js
@@ -1,6 +1,13 @@
 import json5 from "json5";
 
-export default function loadEnvJson(variableName) {
+/**
+ * Returns the value of the specified environment variable after parsing it.
+ *
+ * @param {string} variableName - The name of the environment variable to retrieve.
+ *
+ * @returns {*} The parsed value of the environment variable, or null if it is not defined or cannot be parsed.
+ */
+export default function getEnv(variableName) {
 	if (!(variableName in process.env)) {
 		console.log(`⚠️ `, `Missing ${variableName} environment variable`);
 		return null;
@@ -21,12 +28,17 @@ export default function loadEnvJson(variableName) {
 	}
 
 	try {
-		const ch = parsedConfig[0];
-		if (ch !== "{" && ch !== "[") {
-			parsedConfig = Buffer.from(parsedConfig, "base64").toString("utf8");
-		}
+		// Ends in .json5
+		if (variableName.slice(-6) === ".json5") {
+			const ch = parsedConfig[0];
+			if (ch !== "{" && ch !== "[" && ch !== '"') {
+				parsedConfig = Buffer.from(parsedConfig, "base64").toString(
+					"utf8",
+				);
+			}
 
-		parsedConfig = json5.parse(parsedConfig);
+			parsedConfig = json5.parse(parsedConfig);
+		}
 	} catch (error) {
 		console.log(`⚠️ `, `Failed to parse ${variableName}`);
 		return null;
