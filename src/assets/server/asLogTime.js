821a8b4ee893294f72c4803b11555f774b262cae
Nyaarium <project@nyaarium.com>
2023-10-05 17:57:51 -0700

    docs

--- src/assets/server/asLogTime.js
+++ src/assets/server/asLogTime.js
@@ -1,5 +1,12 @@
 import moment from "moment-timezone";
 
+/**
+ * Converts a date to Unix syslog date format.
+ *
+ * @param {Date} [date] - The date to be converted to Unix syslog date format.
+ *
+ * @returns {string} The date in Unix syslog date format.
+ */
 export default function asLogTime(date = undefined) {
 	return moment(date).toISOString().replace(/T/, " ").replace(/\..*/, "");
 }
