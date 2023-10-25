6cfe6ac687c977e19c431d68a49716bd1eb16ac1
Nyaarium <project@nyaarium.com>
2023-10-25 00:09:02 -0700

    added session Provider and APIs

--- src/pages/api/session/check.js
+++ src/pages/api/session/check.js
@@ -2,12 +2,20 @@ import { createApiHandler, respondJson } from "@/assets/server";
 
 export default createApiHandler({
 	label: __filename,
-	handler: (req, res) => {
+	handler(req, res) {
 		const session = req.session;
+		const user = session?.user;
+		const roles = user?.roles;
 
-		return respondJson(res, {
-			valid: !!session,
-			roles: session?.roles ?? [],
-		});
+		if (roles) {
+			return respondJson(res, {
+				valid: !!session,
+				roles,
+			});
+		} else {
+			return respondJson(res, {
+				valid: false,
+			});
+		}
 	},
 });
--- src/pages/api/session/login.js
+++ src/pages/api/session/login.js
@@ -1,22 +1,25 @@
 import {
 	assertValidLogin,
 	createApiHandler,
+	respondError,
 	respondJson,
 } from "@/assets/server";
 
 export default createApiHandler({
 	label: __filename,
-	handler: async (req, res) => {
+	async handler(req, res) {
 		const { email, password, totp } = req.data;
 
 		const user = await assertValidLogin(email, password, totp);
 
-		await req.attachSession(req, res);
-
 		req.session.user = user;
 
-		await req.session.save();
-
-		return respondJson(res, { test: req?.session });
+		req.session.save((error) => {
+			if (error) {
+				return respondError(req, res, error);
+			} else {
+				return respondJson(res, { ok: true });
+			}
+		});
 	},
 });
