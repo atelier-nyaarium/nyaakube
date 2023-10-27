2428cea0f53b1a7a1594e68a659432502750fbdd
Nyaarium <project@nyaarium.com>
2023-10-26 21:38:56 -0700

    added pg and migration script

---
+++ scripts/migrationGenerate.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo ""
+echo " ✏️  Enter the TypeORM migration name:"
+read migrationName
+
+# Strip non-alphanumeric characters
+migrationName=$(echo "$migrationName" | sed 's/[^a-zA-Z0-9]//g')
+
+# Check if migration name is empty after stripping
+if [ -z "$migrationName" ]; then
+    echo " ❌  Error: Migration name is required"
+    exit 1
+fi
+
+npx typeorm-ts-node-esm \
+	migration:generate \
+	-d "./src/typeorm/cliDataSource.ts" \
+	"src/typeorm/migrations/$migrationName"
