914f18628386f1b97884c3a2a9e3ada2967e26d3
Nyaarium <project@nyaarium.com>
2023-10-27 17:38:54 -0700

    debug

--- src/typeorm/PostgresDataSource.ts
+++ src/typeorm/PostgresDataSource.ts
@@ -1,17 +1,39 @@
 import { DataSource } from "typeorm";
 
+console.log(`NODE_ENV:`, process.env.NODE_ENV);
+
+if (!process.env.POSTGRES_DB) {
+	throw new Error(`POSTGRES_DB variable is missing`);
+}
+if (!process.env.POSTGRES_USER) {
+	throw new Error(`POSTGRES_USER variable is missing`);
+}
+if (!process.env.POSTGRES_PASSWORD) {
+	throw new Error(`POSTGRES_PASSWORD variable is missing`);
+}
+
 let postgresDataSource = null;
 
 export default function PostgresDataSource(configOverrides) {
 	if (postgresDataSource) return postgresDataSource;
 
+	const host = process.env.POSTGRES_HOST || "localhost";
+	const port = Number(process.env.POSTGRES_PORT) || 5432;
+	const username = process.env.POSTGRES_USER;
+	const password = process.env.POSTGRES_PASSWORD;
+	const database = process.env.POSTGRES_DB;
+
+	console.log(
+		` ℹ️  Connecting to: postgresql://${username}:****@${host}:${port}/${database}`,
+	);
+
 	postgresDataSource = new DataSource({
 		type: "postgres",
-		host: process.env.POSTGRES_HOST || "localhost",
-		port: Number(process.env.POSTGRES_PORT) || 5432,
-		username: process.env.POSTGRES_USER,
-		password: process.env.POSTGRES_PASSWORD,
-		database: process.env.POSTGRES_DB,
+		host,
+		port,
+		username,
+		password,
+		database,
 		synchronize: true,
 		logging: true,
 		...configOverrides,
--- src/typeorm/cliDataSource.ts
+++ src/typeorm/cliDataSource.ts
@@ -1,17 +1,4 @@
-import dotenv from "dotenv";
 import PostgresDataSource from "./PostgresDataSource";
-
-console.log(`NODE_ENV:`, process.env.NODE_ENV);
-
-console.log(
-	`Connecting to:`,
-	process.env.POSTGRES_HOST,
-	process.env.POSTGRES_DB,
-);
-
-if (process.env.NODE_ENV !== "production") {
-	dotenv.config();
-}
 
 const datasource = PostgresDataSource({
 	entities: [`src/typeorm/entities/**/*.entity.ts`],
