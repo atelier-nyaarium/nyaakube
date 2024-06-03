460712ca7b0851f81a1fd3e8d0c3d453ba43bf9f
Nyaarium <project@nyaarium.com>
2024-06-03 13:38:50 -0700

    kube debugging

--- scripts/migrationUp.sh
+++ scripts/migrationUp.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
+echo "Connecting to database $POSTGRES_DB on $POSTGRES_HOST as $POSTGRES_USER..."
+
 scripts/loadEnv.sh \
     npx ts-node ./node_modules/typeorm/cli.js \
 	migration:run \
