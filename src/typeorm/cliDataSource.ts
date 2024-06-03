033d07535b8043a6022de3c0eafd7538f2b1aef2
Nyaarium <project@nyaarium.com>
2024-06-03 13:50:04 -0700

    log datasource

--- src/typeorm/cliDataSource.ts
+++ src/typeorm/cliDataSource.ts
@@ -1,8 +1,12 @@
 import PostgresDataSource from "./PostgresDataSource";
 
-const datasource = PostgresDataSource({
+const config = {
 	entities: [`src/typeorm/entities/**/*.entity.ts`],
 	migrations: [`src/typeorm/migrations/**/*.ts`],
-});
+};
+
+console.log(`Creating datasource: ${JSON.stringify(config)}`);
+
+const datasource = PostgresDataSource(config);
 
 export default datasource;
