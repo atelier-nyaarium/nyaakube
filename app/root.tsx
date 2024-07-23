75f8537df98c1555fad8e9be17e2bea4a5e71820
Edward Thang <project@nyaarium.com>
2024-07-23 00:41:14 -0700

    Meta tags

--- app/root.tsx
+++ app/root.tsx
@@ -22,6 +22,8 @@ export default function App(props: { children: ReactNode }) {
 	return (
 		<html>
 			<head>
+				<meta charset="utf-8" />
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
 				<link rel="icon" href="/favicon.ico" />
 				{/* <link rel="manifest" href="/manifest.json" /> */}
 				<Meta />
