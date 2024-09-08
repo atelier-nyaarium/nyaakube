156cbb0a23c6c75a77477e4424784c1c47b390bc
Edward Thang <project@nyaarium.com>
2024-09-08 06:17:25 -0700

    Update renovate-config.js

--- .github/renovate-config.js
+++ .github/renovate-config.js
@@ -19,46 +19,56 @@ module.exports = {
 		{
 			packagePatterns: ["react", "react-dom"],
 			groupName: "react",
+			labels: ["dependencies"],
 		},
 		{
 			packagePatterns: ["next", "eslint-config-next"],
 			groupName: "nextjs",
+			labels: ["dependencies"],
 		},
 		{
 			packagePatterns: ["@remix-run/*"],
 			groupName: "remix",
+			labels: ["dependencies"],
 		},
 		{
 			packagePatterns: ["@mui/material", "@mui/icons-material"],
 			groupName: "material-ui",
+			labels: ["dependencies"],
 		},
 		{
 			packagePatterns: ["@emotion/*"],
 			groupName: "emotion",
+			labels: ["dependencies"],
 		},
 		{
 			packagePatterns: ["@mikro-orm/*"],
 			groupName: "mikro-orm",
+			labels: ["dependencies"],
 		},
 		{
 			packagePatterns: ["@typescript-eslint/eslint-plugin", "@typescript-eslint/parser"],
 			groupName: "eslint",
+			labels: ["dependencies"],
 		},
 		{
 			matchPackagePatterns: ["^github-actions/.*"],
 			groupName: "github-actions",
 			automerge: true,
+			labels: ["dependencies"],
 		},
 		{
 			matchDatasources: ["docker"],
 			groupName: "docker",
 			automerge: true,
+			labels: ["dependencies"],
 		},
 		{
 			matchPackagePatterns: [".*"],
 			matchPaths: [".github/workflows/*.yml"],
 			groupName: "github-workflows",
 			automerge: true,
+			labels: ["dependencies"],
 		},
 	],
 };
